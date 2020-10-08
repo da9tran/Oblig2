0813682d7df7e8eb4777a0fb701c303df5e82d28
Dani <dani2009no@gmail.com>
2020-10-08 15:53:28 +0200

    Oppgave 8d: Implementerte metoden Iterator<T> iterator(int indeks)

--- src/no/oslomet/cs/algdat/DobbeltLenketListe.java
+++ src/no/oslomet/cs/algdat/DobbeltLenketListe.java
@@ -361,7 +361,8 @@ public class DobbeltLenketListe<T> implements Liste<T> {
     }
 
     public Iterator<T> iterator(int indeks) {
-        throw new UnsupportedOperationException();
+        indeksKontroll(indeks, false);
+        return new DobbeltLenketListeIterator(indeks);
     }
 
     private class DobbeltLenketListeIterator implements Iterator<T>
